cfb7af06c0e985eb6e143415a0cb7c87b15998f0
Thiago da Silva de Souza <thiago.dsouza1992@gmail.com>
2022-08-31 11:23:22 -0300

    vidas

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustarTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -16,6 +17,19 @@ function posicaoRandomica() {
 	//remover o mosquito anterior (caso exista)
 	if(document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
+
+
+		//console.log('elemento selecionado foi: v' + vidas)
+
+		if(vidas > 3) {
+			alert('Interromper aplicação - Game over')
+		} else {
+
+		document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+		vidas++
+
+		}
 	}
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -34,6 +48,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function () {
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
 
