0ab7a30871e318a843ebd28fb6ab8afa0a3ba249
Thiago da Silva de Souza <thiago.dsouza1992@gmail.com>
2022-08-31 11:37:13 -0300

    game over

--- jogo.js
+++ jogo.js
@@ -22,7 +22,7 @@ function posicaoRandomica() {
 		//console.log('elemento selecionado foi: v' + vidas)
 
 		if(vidas > 3) {
-			alert('Interromper aplicação - Game over')
+			window.location.href = 'fim_de_jogo.html'
 		} else {
 
 		document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
