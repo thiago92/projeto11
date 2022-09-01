132d6e2259a4a50998b24dc4030bf9c976214eea
Thiago da Silva de Souza <thiago.dsouza1992@gmail.com>
2022-09-01 11:09:53 -0300

    niveis

--- jogo.js
+++ jogo.js
@@ -4,6 +4,22 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '') 
+
+if(nivel === 'normal') {
+	//1500
+	criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+	//1000
+	criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+	criaMosquitoTempo = 750
+	//750
+}
+
 function ajustarTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
